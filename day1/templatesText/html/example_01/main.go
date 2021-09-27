a7dfa1b625b68cb3a46bc41f4e7bc9d240c24af4
Aleksei Solovev <alesolovev@x5.ru>
2021-09-27 17:50:38 +0300

    add week1

---
+++ day1/templatesText/html/example_01/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+const Page = `
+<html>
+<head>
+    <title>Языки программирования, которые знает {{.Name}}: </title>
+</head>
+<body>
+	<h3>Языки программирования, которые знает {{.Name}}: </h3>
+    <ul>
+    {{range .Languages}}<li>{{print .}}</li>{{end}}
+    </ul>
+</body>
+</html>
+`
+
+type UserExperience struct {
+	Name      string
+	Languages []string
+}
+
+func main() {
+	languages := []string{"Go", "C++", "C#"}
+	u := UserExperience{"User", languages}
+
+	t := template.Must(template.New("web").Parse(Page))
+
+	err := t.Execute(os.Stdout, u)
+	if err != nil {
+		panic(err)
+	}
+}
